9db9e45d2b2cbf81d404797e1c5c3d10443311b5
vwmin <fawsuki@gmail.com>
2024-04-15 16:30:49 +0800

    init

---
+++ src/main/kotlin/com/vwmin/onebotvwmin/OnebotVwminApplication.kt
@@ -0,0 +1,11 @@
+package com.vwmin.onebotvwmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnebotVwminApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnebotVwminApplication>(*args)
+}
